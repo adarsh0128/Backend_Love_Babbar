8dc142e3523a70d77ceffbfff12a5528978c01bc
Adarsh kumar <adarshkumar0128@gmail.com>
2025-04-23 14:27:14 +0530

    Auth completed

--- AuthApp/index.js
+++ AuthApp/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 //cookie-parser - what is this and why we need this ?
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require("./config/database").connect();
 
--- AuthApp/routes/user.js
+++ AuthApp/routes/user.js
@@ -2,8 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 const { signup, login } = require("../controllers/auth.js");
+const { auth, isStudent, isAdmin } = require("../middlewares/auth.js");
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/test", auth, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for TESTS",
+  });
+});
+
+//Protected Route
+router.get("/student", auth, isStudent, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for Students",
+  });
+});
+
+router.get("/admin", auth, isAdmin, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for Admin",
+  });
+});
 
 module.exports = router;
